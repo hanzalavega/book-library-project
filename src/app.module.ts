45e29efb43dffae4daf95bd5ab45de3008acc292
hanzalavega <hanzala@pixelvega.com>
2026-06-22 12:05:05 +0600

    chore: install backend dependencies and scaffold core modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { StudentsModule } from './students/students.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, BooksModule],
+  imports: [UsersModule, BooksModule, StudentsModule, OrdersModule],
   controllers: [AppController],
   providers: [],
 })
