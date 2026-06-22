9c7cb8322f75f7fd2146ec10d5605a57fbc9d12d
hanzalavega <hanzala@pixelvega.com>
2026-06-22 09:13:48 +0600

    practiced @Post(), @Body, @Patch, @Delete, @Query, DefaultValuePipe, optional Query on users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,14 @@
 import {
+  Body,
   Controller,
   DefaultValuePipe,
+  Delete,
   Get,
+  Param,
+  ParseBoolPipe,
   ParseIntPipe,
+  Patch,
+  Post,
   Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
@@ -12,16 +18,52 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll(
-    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+  getAllUsersByQuery(
+    @Query('limit', new DefaultValuePipe(1), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
-  ): any {
-    console.log({ limit, page });
-    return this.usersService.getAllUsers();
+    @Query('role') role?: 'admin' | 'intern' | 'engineer',
+  ) {
+    let usersToServe = this.usersService.getAllUsers();
+
+    if (role) {
+      usersToServe = usersToServe.filter((user) => user.role === role);
+    }
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    usersToServe = usersToServe.slice(startIndex, endIndex);
+
+    return usersToServe;
   }
 
-  @Get('single')
-  getSingleUser(): string {
-    return this.usersService.getSingleUser();
+  @Get('intern')
+  getAllInterns() {
+    return this.usersService
+      .getAllUsers()
+      .filter((user) => user.role === 'intern');
+  }
+
+  @Get(':id')
+  getAllInternsWithId(@Param('id', ParseIntPipe) iddd: number) {
+    return this.usersService.getAllUsers().filter((user) => user.id === iddd);
+  }
+
+  @Post()
+  createUser(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dataToBeUpdated: {},
+  ) {
+    return { id, dataToBeUpdated };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return { id };
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,54 +2,57 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  users: [
+  private users = [
     {
-      id: 1;
-      name: 'Rahim Ahmed';
-      email: 'rahim.ahmed@example.com';
-      role: 'member';
-      membershipStatus: 'active';
-      borrowedBooks: 2;
+      id: 1,
+      name: 'Rahim Ahmed',
+      email: 'rahim.ahmed@example.com',
+      role: 'intern',
+      department: 'Frontend',
+      isActive: true,
     },
     {
-      id: 2;
-      name: 'Sara Khan';
-      email: 'sara.khan@example.com';
-      role: 'member';
-      membershipStatus: 'active';
-      borrowedBooks: 0;
+      id: 2,
+      name: 'Sara Khan',
+      email: 'sara.khan@example.com',
+      role: 'engineer',
+      department: 'Backend',
+      isActive: true,
     },
     {
-      id: 3;
-      name: 'Imran Hossain';
-      email: 'imran.hossain@example.com';
-      role: 'librarian';
-      membershipStatus: 'active';
-      borrowedBooks: 0;
+      id: 3,
+      name: 'Imran Hossain',
+      email: 'imran.hossain@example.com',
+      role: 'admin',
+      department: 'Operations',
+      isActive: true,
     },
     {
-      id: 4;
-      name: 'Nadia Islam';
-      email: 'nadia.islam@example.com';
-      role: 'member';
-      membershipStatus: 'inactive';
-      borrowedBooks: 1;
+      id: 4,
+      name: 'Nadia Islam',
+      email: 'nadia.islam@example.com',
+      role: 'engineer',
+      department: 'QA',
+      isActive: false,
     },
     {
-      id: 5;
-      name: 'Tanvir Hasan';
-      email: 'tanvir.hasan@example.com';
-      role: 'member';
-      membershipStatus: 'active';
-      borrowedBooks: 3;
+      id: 5,
+      name: 'Tanvir Hasan',
+      email: 'tanvir.hasan@example.com',
+      role: 'intern',
+      department: 'Design',
+      isActive: true,
+    },
+    {
+      id: 6,
+      name: 'Farhan Ali',
+      email: 'farhan.ali@example.com',
+      role: 'engineer',
+      department: 'DevOps',
+      isActive: true,
     },
   ];
-
   getAllUsers(): any {
     return this.users;
-  }
-
-  getSingleUser() {
-    return 'single user data!!!...!!!';
   }
 }
